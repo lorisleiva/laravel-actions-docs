688e69130d1ecc548c2c93abd03570fe19a42f0d
Loris Leiva <loris.leiva@gmail.com>
2020-04-25 19:52:38 +0100

    Fix npm scripts

---
+++ .vuepress/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    title: 'Laravel Actions',
+    description: 'Documentation for Laravel Actions',
+    head: [
+        ['link', { rel: 'icon', href: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/237/high-voltage-sign_26a1.png' }],
+    ],
+}
