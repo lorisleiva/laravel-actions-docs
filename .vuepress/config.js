eff6d1cf9684e8b11fe1d319104e323cddacab32
Loris Leiva <loris.leiva@gmail.com>
2020-04-27 00:01:42 +0100

    Change accent colour

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    title: 'Laravel Actions',
+    title: '⚡️ Laravel Actions',
     description: 'Documentation for Laravel Actions',
     head: [
         ['link', { rel: 'icon', href: 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/237/high-voltage-sign_26a1.png' }],
